8c3cda5d50d7533319c5baa29279df5fb2232e61
kitifi0 <williamskitisha@gmail.com>
2024-07-26 10:46:12 -0500

    Update Kitisha.py

--- Kitisha.py
+++ Kitisha.py
@@ -1,11 +1,11 @@
-import streamlit as st
+import streamlit as st
 
-# Set the title of the app
-st.title("Welcome to My First Streamlit App")
+#set the title of the app
+st.title("Welcome to my first Streamlit app")
 
-# Add a text input
-name = st.text_input("Enter your name:")
+# Add a text inout
+name = st.text_input("Enter your name:")
 
-# Display the name entered by the user
-if name:
-    st.write(f"Hello, {name}! Welcome to the app.")
+#Display the name entered by the user
+if name:
+  st.write(f"Hello, {name}! Welcome to the app.")
